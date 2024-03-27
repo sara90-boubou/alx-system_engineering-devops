35a90105a3bf02fbd85e379bca88b05c1b743702
sara90-boubou <saraboubou4@gmail.com>
2024-03-27 16:54:19 -0700

    Using Puppet, create a file in /tmp. > > Requirements: > > File path is /tmp/school > File permission is 0744 > File owner is www-data > File group is www-data > File contains I love Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-#Create a file in /tmp
+# creates a file in /tmp
 
-file { 'school':
-ensure  => 'present',
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet'
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
