1cfe04611a3cb1eb1951f70d2eb8adfba4f8b6c7
sara90-boubou <saraboubou4@gmail.com>
2024-05-16 12:28:02 +0000

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,14 @@
-# Fixing the number of failed requests to get to 0
+# Increases the amount of traffic an Nginx server can handle
+
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+#restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
